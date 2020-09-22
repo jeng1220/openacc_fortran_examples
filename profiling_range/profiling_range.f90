4fda68e5893dcd11d965658ace63ec0adc18b7b4
Ryan Jeng <rjeng@nvidia.com>
2020-09-22 21:05:05 +0800

    simplify profiling_range

--- profiling_range/profiling_range.f90
+++ profiling_range/profiling_range.f90
@@ -1,39 +1,27 @@
-subroutine foo()
+program main
     use cudafor
     implicit none
 
-    integer i, j, k, n
-    real, allocatable, contiguous, dimension(:,:) :: data
+    integer i, k, n
+    real, allocatable :: buf(:)
 
-    n = 10000
-    allocate(data(n, n))
-    !$acc enter data create(data(n, n))
+    n = 1000000
+    allocate(buf(n))
 
     do k = 1, 100
-      if (k .eq. 10) then
-        call cudaProfilerStart()
-      end if
+        if (k .eq. 10) then
+            call cudaProfilerStart()
+        end if
 
-      !$acc parallel loop present(data(n, n))
-      do j=1, n
-        !$acc loop independent 
+        !$acc parallel loop copy(buf)
         do i=1, n
-          data(i, j) = data(i, j) + 1
+            buf(i) = buf(i) + 1
         end do
-      end do
 
-
-      if (k .eq. 20) then
-        call cudaProfilerStop()
-      end if
+        if (k .eq. 20) then
+            call cudaProfilerStop()
+        end if
     end do
 
-    !$acc exit data delete(data)
-    deallocate(data)
-end subroutine
-
-program main
-    implicit none
-    call foo
+    deallocate(buf)
 end program main
-
