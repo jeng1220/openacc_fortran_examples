e78f824ab2fb0d4ec3821e97ad6ac0c9585b0b71
Ryan Jeng <rjeng@nvidia.com>
2020-09-23 14:27:12 +0800

    refactor

--- access_efficiency/access_efficiency.f90
+++ access_efficiency/access_efficiency.f90
@@ -1,140 +1,125 @@
 subroutine acc_loop_2d()
     implicit none
     integer i, j, n
-    real, allocatable, contiguous, dimension(:,:) :: data
+    real, allocatable :: buf(:, :)
     real start, finish
 
     n = 10000
-    allocate(data(n, n))
-    !$acc enter data create(data(n, n))
+    allocate(buf(n, n))
+    !$acc enter data create(buf)
 
     print*, "2D access:"
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n))
+    !$acc parallel loop present(buf) collapse(2)
     do j=1, n
-      !$acc loop independent 
-      do i=1, n
-        data(i, j) = data(i, j) + 1
-      end do
+    do i=1, n
+        buf(i, j) = buf(i, j) + 1
+    end do
     end do
     call cpu_time(finish)
     print '("I, J order time = ",f6.3," seconds.")', (finish - start)
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n)) 
+    !$acc parallel loop present(buf) collapse(2)
     do i=1, n
-      !$acc loop independent 
-      do j=1, n
-        data(i, j) = data(i, j) + 1
-      end do
+    do j=1, n
+        buf(i, j) = buf(i, j) + 1
+    end do
     end do
     call cpu_time(finish)
     print '("J, I order time = ",f6.3," seconds.")', (finish - start)
 
-    !$acc exit data delete(data)
-    deallocate(data)
+    !$acc exit data delete(buf)
+    deallocate(buf)
 end subroutine
 
 subroutine acc_loop_3d()
     implicit none
-    integer i, j, k
-    integer n
-    real, allocatable, contiguous, dimension(:,:,:) :: data
+    integer i, j, k, n
+    real, allocatable :: buf(:, :, :)
     real start, finish
     n = 1000
-    allocate(data(n, n, n))
-    !$acc enter data create(data(n, n, n))
+    allocate(buf(n, n, n))
+    !$acc enter data create(buf)
 
     print*, "3D access:"
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n, n)) 
+    !$acc parallel loop present(buf) collapse(3)
     do k=1, n
-      !$acc loop independent worker 
-      do j=1, n
-        !$acc loop independent vector
-        do i=1, n
-          data(i, j, k) = data(i, j, k) + 1
-        end do
-      end do
+    do j=1, n
+    do i=1, n
+        buf(i, j, k) = buf(i, j, k) + 1
+    end do
+    end do
     end do
     call cpu_time(finish)
     print '("I, J, K order time = ",f6.3," seconds.")', (finish - start)
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n, n)) 
+    !$acc parallel loop present(buf) collapse(3)
     do j=1, n
-      !$acc loop independent worker
-      do k=1, n
-        !$acc loop independent vector
-        do i=1, n
-          data(i, j, k) = data(i, j, k) + 1
-        end do
-      end do
+    do k=1, n
+    do i=1, n
+        buf(i, j, k) = buf(i, j, k) + 1
+    end do
+    end do
     end do
     call cpu_time(finish)
     print '("I, K, J order time = ",f6.3," seconds.")', (finish - start)
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n, n)) 
+    !$acc parallel loop present(buf) collapse(3)
     do i=1, n
-      !$acc loop independent worker
-      do k=1, n
-        !$acc loop independent vector
-        do j=1, n
-          data(i, j, k) = data(i, j, k) + 1
-        end do
-      end do
+    do k=1, n
+    do j=1, n
+          buf(i, j, k) = buf(i, j, k) + 1
+    end do
+    end do
     end do
     call cpu_time(finish)
     print '("J, K, I order time = ",f6.3," seconds.")', (finish - start)
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n, n)) 
+    !$acc parallel loop present(buf) collapse(3)
     do k=1, n
-      !$acc loop independent worker
-      do i=1, n
-        !$acc loop independent vector
-        do j=1, n
-          data(i, j, k) = data(i, j, k) + 1
-        end do
-      end do
+    do i=1, n
+    do j=1, n
+        buf(i, j, k) = buf(i, j, k) + 1
+    end do
+    end do
     end do
     call cpu_time(finish)
     print '("J, I, K order time = ",f6.3," seconds.")', (finish - start)
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n, n)) 
+    !$acc parallel loop present(buf) collapse(3)
     do i=1, n
-      !$acc loop independent worker
-      do j=1, n
-        !$acc loop independent vector
-        do k=1, n
-          data(i, j, k) = data(i, j, k) + 1
-        end do
-      end do
+    do j=1, n
+    do k=1, n
+        buf(i, j, k) = buf(i, j, k) + 1
+    end do
+    end do
     end do
     call cpu_time(finish)
     print '("K, J, I order time = ",f6.3," seconds.")', (finish - start)
 
     call cpu_time(start)
-    !$acc parallel loop present(data(n, n, n)) 
+    !$acc parallel loop present(buf) collapse(3)
     do j=1, n
-      !$acc loop independent worker
-      do i=1, n
-        !$acc loop independent vector
-        do k=1, n
-          data(i, j, k) = data(i, j, k) + 1
-        end do
-      end do
+    do i=1, n
+    do k=1, n
+        buf(i, j, k) = buf(i, j, k) + 1
+    end do
+    end do
     end do
     call cpu_time(finish)
     print '("K, I, J order time = ",f6.3," seconds.")', (finish - start)
 
-    !$acc exit data delete(data)
+    !$acc exit data delete(buf)
 
-    deallocate(data)
+    deallocate(buf)
 end subroutine
 
 program main
