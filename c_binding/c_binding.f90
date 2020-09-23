3417f79e8dca38acd77d0ee26ca34b668a74fafa
Ryan Jeng <rjeng@nvidia.com>
2020-09-23 13:46:02 +0800

    refactor

--- c_binding/c_binding.f90
+++ c_binding/c_binding.f90
@@ -1,39 +1,33 @@
-program test_call_C
-
+program main
   use iso_c_binding
-
   implicit none
 
   interface c_interface
-
-    subroutine get_filled_ar(ar, h, w) bind(C, name="get_filled_ar")
-
+    subroutine my_c_func(buf, w, h) bind(C, name="my_c_func")
       use iso_c_binding
-
       implicit none
 
-      integer(c_int), intent(out), dimension(*) :: ar
-      integer(c_int), intent(out) :: h, w
-
-    end subroutine get_filled_ar
-
+      integer(c_int), intent(out), dimension(*) :: buf
+      integer(c_int), intent(out) :: w, h
+    end subroutine my_c_func
   end interface c_interface
 
-  integer(c_int), dimension(3, 5) :: ar
+  integer(c_int), allocatable :: buf(:, :)
   integer(c_int) :: x, y, w, h
 
   w = 5
   h = 3
+  allocate(buf(w, h))
 
-  do x = 1, w
   do y = 1, h
-    ar(y, x) = (y-1)*5+(x-1)
+  do x = 1, w
+    buf(x, y) = (y - 1) * w + (x - 1)
   end do
   end do
 
-  print *, 'print from fortran:'
-  print *, ar
+  print *, 'print from fortran:\n', buf
+  call my_c_func(buf, w, h)
+  print *, 'after C function, print from fortran:\n', buf
 
-  call get_filled_ar(ar, h, w)
-
-end program test_call_C
+  deallocate(buf)
+end program main
--- c_binding/c_func.c
+++ c_binding/c_func.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-void get_filled_ar(int *ar, int *hptr, int *wptr) 
+void my_c_func(int *buf, int *wptr, int *hptr)
 {
   int h = *hptr;
   int w = *wptr;
 
-  printf("print from c, h=%d, w=%d\n", h, w);
+  printf("print from c, w=%d, h=%d\n", w, h);
 
   for (int x = 0; x < w; ++x) {
     for (int y = 0; y < h; ++y) { 
-      printf("%d, ", ar[ x * h + y ]);
+      printf("%d, ", buf[ x * h + y ]);
     }
     printf("\n");
   }
   printf("\n");
+
+  #pragma acc parallel loop copy(buf[w*h])
+  for (int x = 0; x < w; ++x) {
+    #pragma acc loop
+    for (int y = 0; y < h; ++y) { 
+      buf[ x * h + y ] += 1;
+    }
+  }
 
   return;
 }
